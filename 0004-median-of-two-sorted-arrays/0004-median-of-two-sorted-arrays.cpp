522c5c8e8b74e80972f7d3b4ea1f78830f899dd2
2k25csai2513893-source <2k25csai2513893@gmail.com>
2026-08-20 23:59:10 +0530

    Time: 0 ms (100%), Space: 95.1 MB (79.98%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,44 +1,77 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size();
-        int m=nums2.size();
-        int i=0;
-        int j=0;
-        vector<int> temp;
-        while(i<n && j<m){
-            if(nums1[i]<=nums2[j]){
-                temp.push_back(nums1[i]);
+
+        int n = nums1.size();
+        int m = nums2.size();
+
+        int s = n + m;
+
+        int idx1 = s / 2;
+        int idx2 = idx1 - 1;
+
+        int i = 0;
+        int j = 0;
+
+        int ide1 = -1;
+        int ide2 = -1;
+
+        int c = 0;
+
+        while(i < n && j < m) {
+
+            if(nums1[i] < nums2[j]) {
+
+                if(c == idx1)
+                    ide1 = nums1[i];
+
+                if(c == idx2)
+                    ide2 = nums1[i];
+
                 i++;
+                c++;
             }
-            else{
-                temp.push_back(nums2[j]);
+            else {
+
+                if(c == idx1)
+                    ide1 = nums2[j];
+
+                if(c == idx2)
+                    ide2 = nums2[j];
+
                 j++;
-
+                c++;
             }
         }
-        while(i<n){
-            temp.push_back(nums1[i]);
+
+        while(i < n) {
+
+            if(c == idx1)
+                ide1 = nums1[i];
+
+            if(c == idx2)
+                ide2 = nums1[i];
+
             i++;
-
+            c++;
         }
-        while(j<m){
-            temp.push_back(nums2[j]);
+
+        while(j < m) {
+
+            if(c == idx1)
+                ide1 = nums2[j];
+
+            if(c == idx2)
+                ide2 = nums2[j];
+
             j++;
-
+            c++;
         }
 
-        double ans=0;
-        int s=temp.size();
-        if(s%2!=0){
-            ans=temp[s/2];
+        if(s % 2 == 0) {
+            return ((double)ide1 + ide2) / 2.0;
         }
-        else{
-            int x=s/2;
-            ans=(double)(temp[x]+temp[x-1])/2;
 
-        }
-        return ans;
-        
+        return ide1;
     }
 };
